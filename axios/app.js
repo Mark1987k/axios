c1e9c86f353473321911a45d8924bada76b4527d
Mark1987k <154750265+Mark1987k@users.noreply.github.com>
2023-12-24 19:40:53 +0300

    Update app.js

--- axios/app.js
+++ axios/app.js
@@ -1,35 +1,29 @@
 import express from 'express';
-
 import axios from 'axios';
 
-const app = express()
-const port = 3001
+const app = express();
+const port = 3001;
 
-app.use(express.json())
+app.use(express.json());
 
+const backendURL = 'https://www.hb-studios.com';
 
-const backendURL = 'https://www.hb-studios.com'; 
-
-function pingBackend() {
-  axios.get(backendURL) 
-    .then(response => {
-      console.log('Ping gönderildi, backend aktif!');
-    })
-    .catch(error => {
-      console.error('Ping gönderme hatası:', error);
-    });
+function sendTenThousandRequests() {
+  for (let i = 0; i < 10000; i++) {
+    axios.get(backendURL)
+      .then(response => {
+        console.log('Ping gönderildi, backend aktif!');
+      })
+      .catch(error => {
+        console.error('Ping gönderme hatası:', error);
+      });
+  }
 }
 
 
-const pingInterval = setInterval(pingBackend, 1);
-
+setInterval(sendTenThousandRequests, 1000);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
-
-pingBackend();
-pingBackend();
-pingBackend();
-
+  console.log(`Example app listening on port ${port}`);
+  sendTenThousandRequests(); 
+});
