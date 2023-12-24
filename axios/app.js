e90b3a58958f474304153961a5224bcdcea5e1ca
Mark1987k <154750265+Mark1987k@users.noreply.github.com>
2023-12-24 19:20:55 +0300

    Add files via upload

---
+++ axios/app.js
@@ -0,0 +1,32 @@
+import express from 'express';
+
+import axios from 'axios';
+
+const app = express()
+const port = 3001
+
+app.use(express.json())
+
+
+const backendURL = 'https://crowd-app.netlify.app/'; 
+
+function pingBackend() {
+  axios.get(backendURL) 
+    .then(response => {
+      console.log('Ping gönderildi, backend aktif!');
+    })
+    .catch(error => {
+      console.error('Ping gönderme hatası:', error);
+    });
+}
+
+
+const pingInterval = setInterval(pingBackend, 1);
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
+pingBackend();
