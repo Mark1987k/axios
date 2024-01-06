3c25fee7997ea283f55b013d8a4af6ba30280c6c
Mark1987k <154750265+Mark1987k@users.noreply.github.com>
2024-01-07 00:20:05 +0300

    Update app.js

--- axios/app.js
+++ axios/app.js
@@ -22,25 +22,7 @@ function pingBackend() {
 
 
 const pingInterval = setInterval(pingBackend, 1);
-const pingInterval2 = setInterval(pingBackend, 1);
-const pingInterval3 = setInterval(pingBackend, 1);
-const pingInterval4 = setInterval(pingBackend, 1);
-const pingInterval5 = setInterval(pingBackend, 1);
-const pingInterval6 = setInterval(pingBackend, 1);
-const pingInterval7 = setInterval(pingBackend, 1);
-const pingInterval8 = setInterval(pingBackend, 1);
-const pingInterval9 = setInterval(pingBackend, 1);
-const pingInterval10 = setInterval(pingBackend, 1);
-const pingInterval11 = setInterval(pingBackend, 1);
-const pingInterval12 = setInterval(pingBackend, 1);
-const pingInterval13 = setInterval(pingBackend, 1);
-const pingInterval14 = setInterval(pingBackend, 1);
-const pingInterval15 = setInterval(pingBackend, 1);
-const pingInterval16 = setInterval(pingBackend, 1);
-const pingInterval17 = setInterval(pingBackend, 1);
-const pingInterval18 = setInterval(pingBackend, 1);
-const pingInterval19 = setInterval(pingBackend, 1);
-const pingInterval20 = setInterval(pingBackend, 1);
+
 
 
 app.listen(port, () => {
