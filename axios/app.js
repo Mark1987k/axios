5ab24f882e1e3864d3d62aa19ec0959cb5f16e8b
Mark1987k <154750265+Mark1987k@users.noreply.github.com>
2024-01-07 00:37:40 +0300

    Update app.js

--- axios/app.js
+++ axios/app.js
@@ -8,7 +8,7 @@ const port = 3001
 app.use(express.json())
 
 
-const backendURL = 'https://www.hb-studios.com'; 
+const backendURL = 'https://cex.io'; 
 
 function pingBackend() {
   axios.get(backendURL) 
