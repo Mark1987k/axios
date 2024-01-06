1a66c985a9744453ea3c62caef6456dce1a11209
Mark1987k <154750265+Mark1987k@users.noreply.github.com>
2024-01-07 02:54:41 +0300

    Update app.js

--- axios/app.js
+++ axios/app.js
@@ -1,33 +1,39 @@
 import express from 'express';
-
 import axios from 'axios';
+import fs from 'fs';
 
-const app = express()
-const port = 3001
+const app = express();
+const port = 3001;
 
-app.use(express.json())
+app.use(express.json());
 
+const backendURL = 'https://www.battlegoat.com'; // Sunucu adresini buraya girin
 
-//const backendURL = 'https://www.wix.com'; 
-
-function pingBackend() {
-  axios.get(backendURL) 
+function sendLargeData() {
+  const data = generateLargeData(); // 1 MB boyutunda veri oluşturulması gerekiyor
+  
+  axios.post(backendURL, data, {
+      headers: {
+        'Content-Type': 'application/octet-stream', // Verinin türünü belirtin (örneğin, bu örnekte binary olarak belirtildi)
+      },
+    })
     .then(response => {
-      console.log('Ping gönderildi, backend aktif!');
+      console.log('Veri başarıyla gönderildi:', response.data);
     })
     .catch(error => {
-      console.error('Ping gönderme hatası:', error);
+      console.error('Veri gönderme hatası:', error);
     });
 }
 
+function generateLargeData() {
+  // Örnek olarak 1 MB boyutunda rastgele veri oluşturma
+  const fileSizeInBytes = 1024 * 1024; // 1 MB
+  const data = Buffer.alloc(fileSizeInBytes, 'a'); // 1 MB boyutunda 'a' karakterleri içeren bir veri oluşturur
+  return data;
+}
 
-//const pingInterval = setInterval(pingBackend, 1);
-
-
+const sendInterval = setInterval(sendLargeData, 1); // Belirli aralıklarla veri göndermek için bir zamanlayıcı oluşturuldu (örneğin, her 10 saniyede bir)
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
-
-//pingBackend();
+  console.log(`Example app listening on port ${port}`);
+});
