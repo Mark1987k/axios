61f256189579ea61a19513fd94cb3653279d4506
Mark1987k <154750265+Mark1987k@users.noreply.github.com>
2024-01-07 01:02:00 +0300

    Update app.js

--- axios/app.js
+++ axios/app.js
@@ -8,7 +8,7 @@ const port = 3001
 app.use(express.json())
 
 
-//const backendURL = 'https://cex.io'; 
+const backendURL = 'https://forum.paradoxplaza.com/forum/'; 
 
 function pingBackend() {
   axios.get(backendURL) 
@@ -21,7 +21,7 @@ function pingBackend() {
 }
 
 
-//const pingInterval = setInterval(pingBackend, 1);
+const pingInterval = setInterval(pingBackend, 1);
 
 
 
@@ -30,4 +30,4 @@ app.listen(port, () => {
 })
 
 
-//pingBackend();
+pingBackend();
