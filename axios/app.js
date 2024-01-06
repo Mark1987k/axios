5a6431581ba4798299a6a3a71df21a1edfbb0979
Mark1987k <154750265+Mark1987k@users.noreply.github.com>
2024-01-07 01:20:02 +0300

    Update app.js

--- axios/app.js
+++ axios/app.js
@@ -8,7 +8,7 @@ const port = 3001
 app.use(express.json())
 
 
-const backendURL = 'https://www.paradoxinteractive.com'; 
+const backendURL = 'https://www.jackboxgames.com'; 
 
 function pingBackend() {
   axios.get(backendURL) 
