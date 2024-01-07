f3f2daa5c6b9bfbf5e3bd9f2dab16367afea0425
Mark1987k <154750265+Mark1987k@users.noreply.github.com>
2024-01-07 03:08:11 +0300

    Update app.js

--- axios/app.js
+++ axios/app.js
@@ -27,7 +27,7 @@ function sendLargeData() {
 
 function generateLargeData() {
   // Örnek olarak 1 MB boyutunda rastgele veri oluşturma
-  const fileSizeInBytes = 1024 * 1024; // 1 MB
+  const fileSizeInBytes = 10240 * 10240; // 10MB
   const data = Buffer.alloc(fileSizeInBytes, 'a'); // 1 MB boyutunda 'a' karakterleri içeren bir veri oluşturur
   return data;
 }
